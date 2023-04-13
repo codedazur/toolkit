3dcc4a792df95830053af1cc087226d04a63cbae
Nadine van Leeuwen <nadinevanleeuwen@codedazur.com>
2023-04-13 15:27:12 +0200

    Update WithApp.tsx (#67)

--- apps/storybook/decorators/WithApp.tsx
+++ apps/storybook/decorators/WithApp.tsx
@@ -1,9 +1,13 @@
-import { App } from "@codedazur/react-components";
+import { App, Background, Padding } from "@codedazur/react-components";
 import { DecoratorFn } from "@storybook/react";
 import { storyTheme } from "../themes/storyTheme";
 
 export const WithApp: DecoratorFn = (Story) => (
   <App rootSelector="#root" theme={storyTheme}>
-    <Story />
+    <Background>
+      <Padding padding="1.5rem">
+        <Story />
+      </Padding>
+    </Background>
   </App>
 );
