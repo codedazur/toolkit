9f97bc956f25eddcc95fae4affdb7e58fa7ae153
Serdar Sayin <serdar.sayin@polestar.com>
2023-04-13 15:04:27 +0200

    add tests for omit and pick

--- packages/essentials/utilities/object/pick.ts
+++ packages/essentials/utilities/object/pick.ts
@@ -1,3 +1,11 @@
+/**
+ * This function takes an object and an array of keys as arguments and returns a new object with only the keys from the array. The function is generic so it can be used with any type of object. 
+ * @param record - An object
+ * @param keys  - An array of keys to pick from the object
+ * @returns 
+ * pick({ a: 1, b: 2, c: 3 }, ["a"]); // { a: 1 }
+ */
+
 export function pick<T extends object, K extends keyof T>(
   record: T,
   keys: K[]
