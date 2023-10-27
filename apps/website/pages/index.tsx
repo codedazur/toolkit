21e0fed0ef829aa9c986512d038c13f82dac4481
Serdar Sayin <serdar.sayin@polestar.com>
2023-10-27 11:17:55 +0200

    fix(react-essentials):make resolveMaybeRef ssrsafe

--- apps/website/pages/index.tsx
+++ apps/website/pages/index.tsx
@@ -1,15 +1,21 @@
-import {
-  AspectRatio,
-  Center,
-  ConstrainedBox,
-  Page,
-} from "@codedazur/react-components";
+import { Center, ConstrainedBox } from "@codedazur/react-components";
 import { Logo } from "@apps/website/components/Logo";
+import { useMedia } from "@codedazur/react-media";
+import { resolveMaybeRef } from "@codedazur/react-essentials";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
+  const { setVolume } = useMedia();
+  setVolume(0.5);
+  const elementRef = useRef(null);
+  const element = resolveMaybeRef(elementRef);
+  useEffect(() => {
+    console.log(element);
+  }, [element]);
+
   return (
     <Center>
-      <ConstrainedBox width="50%" maxWidth="20rem">
+      <ConstrainedBox ref={elementRef} width="50%" maxWidth="20rem">
         <Logo />
       </ConstrainedBox>
     </Center>
--- packages/react-essentials/utilities/resolveMaybeRef.ts
+++ packages/react-essentials/utilities/resolveMaybeRef.ts
@@ -3,5 +3,19 @@ import { MaybeRef } from "../types/MaybeRef";
 export function resolveMaybeRef<T extends HTMLElement>(
   ref: MaybeRef<T>,
 ): T | null {
-  return ref instanceof HTMLElement ? ref : ref?.current ?? null;
+  if (
+    typeof window !== "undefined" &&
+    "HTMLElement" in window &&
+    ref instanceof window.HTMLElement
+  ) {
+    return ref;
+  } else if (
+    ref &&
+    typeof ref === "object" &&
+    "current" in ref &&
+    ref.current
+  ) {
+    return ref.current as T;
+  }
+  return null;
 }
