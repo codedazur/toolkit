4be212f59f3f397695006c289cc163117a7dbe1a
Thijs Daniels <thijsdaniels@codedazur.com>
2024-04-29 19:35:52 +0200

    docs: update pick and omit descriptions

--- packages/essentials/utilities/object/pick.ts
+++ packages/essentials/utilities/object/pick.ts
@@ -1,11 +1,8 @@
 /**
- * This function takes an object and an array of keys as arguments and returns a new object with only the keys from the array. The function is generic so it can be used with any type of object.
- * @param record - An object
- * @param keys  - An array of keys to pick from the object
- * @returns
- * pick({ a: 1, b: 2, c: 3 }, ["a"]); // { a: 1 }
+ * Copies an object, keeping only the specified keys and discarding the rest.
+ *
+ * @example pick({ a: 1, b: 2, c: 3 }, ["a"]) = { a: 1 }
  */
-
 export function pick<T extends object, K extends keyof T>(
   record: T,
   keys: K[],
