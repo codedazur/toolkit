51531abe89589bc2daf07780a138e39eb0476821
Thijs Daniels <thijsdaniels@codedazur.com>
2025-06-17 15:18:37 +0200

    chore: deprecate sanity site

---
+++ .changeset/solid-mugs-wish.md
@@ -0,0 +1,5 @@
+---
+"@codedazur/cdk-sanity-site": patch
+---
+
+The SanitySite has been marked as deprecated in favor of the StaticSite with the SinglePage rewrite mode.
--- packages/cdk-sanity-site/src/constructs/SanitySite.ts
+++ packages/cdk-sanity-site/src/constructs/SanitySite.ts
@@ -1,34 +1,21 @@
-import { StaticSite, StaticSiteProps } from "@codedazur/cdk-static-site";
-import { FunctionCode } from "aws-cdk-lib/aws-cloudfront";
+import {
+  RewriteMode,
+  StaticSite,
+  StaticSiteProps,
+} from "@codedazur/cdk-static-site";
 import { Construct } from "constructs";
 
-export interface SanitySiteProps extends StaticSiteProps {}
+export interface SanitySiteProps extends Omit<StaticSiteProps, "rewriteMode"> {}
 
+/**
+ * @deprecated Please use the `StaticSite` construct instead with the
+ * `rewriteMode` set to `RewriteMode.SinglePage`.
+ */
 export class SanitySite extends StaticSite {
   constructor(scope: Construct, id: string, props: SanitySiteProps) {
     super(scope, id, {
       ...props,
-      distribution: {
-        ...props.distribution,
-        functions: {
-          ...props.distribution?.functions,
-          viewerRequest: [
-            ...(props.distribution?.functions?.viewerRequest ?? []),
-            FunctionCode.fromInline(/* js */ `
-              function rewriteToIndex(event, next) {
-                var isStaticPattern = new RegExp("^/static/");
-                var isStatic = isStaticPattern.test(event.request.uri);
-
-                if (!isStatic) {
-                  event.request.uri = "/index.html";
-                }
-
-                return next(event);
-              }
-            `),
-          ],
-        },
-      },
+      rewriteMode: RewriteMode.SinglePage,
     });
   }
 }
