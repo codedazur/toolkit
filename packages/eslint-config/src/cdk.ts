abd7eec86659047efc64f07d7de59545e17f354e
Thijs Daniels <thijsdaniels@codedazur.com>
2026-01-20 19:45:02 +0100

    chore: skip eslint dependencies bundle

--- packages/eslint-config/src/cdk.ts
+++ packages/eslint-config/src/cdk.ts
@@ -1,10 +1,16 @@
 import cdkPlugin from "eslint-plugin-awscdk";
 import { Config, defineConfig } from "eslint/config";
 
-export const cdk = defineConfig(cdkPlugin.configs.recommended as Config[], {
-  rules: {
-    "awscdk/no-parent-name-construct-id-match": "off",
-    "awscdk/construct-constructor-property": "off",
-    "awscdk/no-unused-props": "off",
+export const cdk = defineConfig(
+  cdkPlugin.configs.recommended as Config[],
+  {
+    rules: {
+      "awscdk/no-parent-name-construct-id-match": "off",
+      "awscdk/construct-constructor-property": "off",
+      "awscdk/no-unused-props": "off",
+    },
   },
-});
+  {
+    ignores: ["cdk.out/**/*"],
+  },
+);
--- packages/eslint-config/tsup.config.ts
+++ packages/eslint-config/tsup.config.ts
@@ -12,4 +12,5 @@ export default defineConfig({
   format: "esm",
   dts: true,
   clean: true,
+  skipNodeModulesBundle: true,
 });
