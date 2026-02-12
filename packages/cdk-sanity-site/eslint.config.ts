2cfc839ecdf35eff5eb8ec9e28cdde263d7f3e38
Thijs Daniels <thijsdaniels@codedazur.com>
2026-02-12 13:41:12 +0100

    chore: remove sanity site

--- packages/cdk-sanity-site/eslint.config.ts
+++
@@ -1,6 +0,0 @@
-import { distribution } from "@codedazur/eslint-config";
-import { cdk } from "@codedazur/eslint-config/cdk";
-import { defineConfig } from "eslint/config";
-import root from "../../eslint.config";
-
-export default defineConfig(root, cdk, distribution);
